80c5059e02841919c2d4fa2839f5339be5f571ed
iHannibal <fredisstillalmighty@gmail.com>
2016-04-01 23:21:16 +0800

    First commit.

---
+++ FYBlocksiniOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYBlocksiniOS
+//
+//  Created by Frankenstein Yang on 4/1/16.
+//  Copyright © 2016 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYBlocksiniOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYBlocksiniOS
+//
+//  Created by Frankenstein Yang on 4/1/16.
+//  Copyright © 2016 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
