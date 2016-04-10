c67d8a3887789a72c26e6c160bfe777e410cc46e
yangjinzhong <fredisstillalmighty@gmail.com>
2016-04-10 22:51:55 +0800

    Test.

--- FYBlocksiniOS/FYSecondController.m
+++ FYBlocksiniOS/FYSecondController.m
@@ -12,8 +12,19 @@
 
 - (void)viewDidLoad {
     
-    [self dismissViewControllerAnimated:YES completion:nil];
+    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    backBtn.frame = CGRectMake(20, 100, 100, 50);
+    backBtn.backgroundColor = [UIColor redColor];
+    [backBtn setTitle:@"Back" forState:UIControlStateNormal];
+    [backBtn addTarget:self
+                    action:@selector(backBtnClicked)
+          forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:backBtn];
     
+}
+
+- (void)backBtnClicked {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)dealloc {
