642d3603abb14e3e0f8e99c470bd7d95256563c3
yangjinzhong <fredisstillalmighty@gmail.com>
2016-05-02 23:03:27 +0800

    New idea.

---
+++ FYBlocksiniOS/View/FYIdeas/FYNewBanner.h
@@ -0,0 +1,13 @@
+//
+//  FYNewBanner.h
+//  FYBlocksiniOS
+//
+//  Created by Frankenstein Yang on 5/2/16.
+//  Copyright © 2016 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FYNewBanner : UIView
+
+@end
---
+++ FYBlocksiniOS/View/FYIdeas/FYNewBanner.m
@@ -0,0 +1,13 @@
+//
+//  FYNewBanner.m
+//  FYBlocksiniOS
+//
+//  Created by Frankenstein Yang on 5/2/16.
+//  Copyright © 2016 Frankenstein Yang. All rights reserved.
+//
+
+#import "FYNewBanner.h"
+
+@implementation FYNewBanner
+
+@end
